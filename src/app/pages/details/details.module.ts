c0c54e0b9940e1d254284ddfb1c335ebb24d265e
Henrique Luiz <heenluy@gmail.com>
2022-07-13 20:44:16 -0300

    feat: criação do componente 'details' para feature ver detalhes

--- src/app/pages/details/details.module.ts
+++ src/app/pages/details/details.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { DetailsPage } from './containers/details/details.page';
+import { DetailsGuard } from './services/details.guard';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailsPage
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: DetailsPage, title: 'Cloudy App — Detalhes', canActivate: [DetailsGuard] }
+    ])
+  ],
+  providers: [
+    DetailsGuard
   ]
 })
 export class DetailsModule { }
